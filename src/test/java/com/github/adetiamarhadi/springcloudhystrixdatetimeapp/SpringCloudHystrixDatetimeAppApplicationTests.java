4493aaad444ff8e9eceb2c2069f77c8413753c61
adetiamarhadi <adetia.marhadi@gmail.com>
2020-08-17 14:40:20 +0700

    demo spring cloud hystrix with turbine

---
+++ src/test/java/com/github/adetiamarhadi/springcloudhystrixdatetimeapp/SpringCloudHystrixDatetimeAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.adetiamarhadi.springcloudhystrixdatetimeapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudHystrixDatetimeAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
